d80caf428835fcf964e920de50ffc184caff9a47
Heitor Silva <heitorsilvanascimento@hotmail.com>
2021-02-27 21:36:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior_que__menor_que
+{
+    class Maior 
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 20;
+            int c = 2;
+
+
+            if (a < b)
+            {
+                Console.WriteLine("É verdade!");
+            }
+            else if (a > c)
+            {
+                Console.WriteLine("!Executou Elseif");
+            }
+            
+            else
+            {
+                Console.WriteLine("É mentira!");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
